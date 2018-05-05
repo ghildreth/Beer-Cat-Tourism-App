6acea6fb6bb04ea973b26de96416429d6de63695
Sadie <sadiefreeman@gmail.com>
2018-05-05 01:33:29 +0000

    Added Beer component

---
+++ react_client/beercat_app/src/Beer.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+
+export default class Beer extends Component {
+  render() {
+    console.log('Rendering <Beer />');
+
+    return (
+      <div>
+        <h2>Beer</h2>
+        <h5>Description:</h5>
+        <h5>Style/Type:</h5>
+        <h5>ABV:</h5>
+        <h5>SRM:</h5>
+        <h5>IBU:</h5>
+      </div>
+    );
+  }
+}
