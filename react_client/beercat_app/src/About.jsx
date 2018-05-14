5bbec7115f10299ca01e167c84e126ea3bf17635
mattwong59 <mattwong59@gmail.com>
2018-05-14 20:19:56 +0000

    Add margin-top to content on /about

--- react_client/beercat_app/src/About.jsx
+++ react_client/beercat_app/src/About.jsx
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 export default class About extends Component {
   render() {
     return (
-      <div>
+      <div className="about-content">
         <h2>Welcome to Beer Cat</h2>
         <p>Get to know Beer Cat, your personalized guide to finding the best craft beer around the world.</p>
       </div>
