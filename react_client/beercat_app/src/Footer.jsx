7750aa77f65b337a0063ee3b05af461010ff1fed
mattwong59 <mattwong59@gmail.com>
2018-05-15 01:54:38 +0000

    Add footer in progress on Single Tour page

---
+++ react_client/beercat_app/src/Footer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { render } from "react-dom";
+
+const footerStyle = {
+  backgroundColor: "efefef",
+  fontSize: "20px",
+  color: "black",
+  borderTop: "1px solid #E7E7E7",
+  textAlign: "center",
+  padding: "20px",
+  position: "fixed",
+  left: "0",
+  bottom: "0",
+  height: "60px",
+  width: "100%"
+};
+
+const phantomStyle = {
+  display: "block",
+  padding: "20px",
+  height: "60px",
+  width: "100%"
+};
+
+function Footer({ children }) {
+  return (
+    <div>
+      <div style={phantomStyle} />
+      <div style={footerStyle}>{children}</div>
+    </div>
+  );
+}
+
+export default Footer
--- react_client/beercat_app/src/SingleTour.jsx
+++ react_client/beercat_app/src/SingleTour.jsx
@@ -6,6 +6,7 @@ import Map from "./Map.jsx";
 import axios from 'axios';
 import Rating from 'react-rating';
 import Alert from 'react-s-alert';
+import Footer from './Footer.jsx'
 import './styles/alert-default.css';
 import 'react-s-alert/dist/s-alert-css-effects/jelly.css';
 import Slider from 'react-animated-slider';
@@ -99,18 +100,19 @@ export default class SingleTour extends Component {
             </span>
             <p className="tour-description">{tour.full_description}</p><br/>
             <span className="brewery-route"><strong>Stops on the Tour:</strong></span>
-            <Slider autoplay="1500" className="slider tour-slider">        
-              {this.state.tour.breweries.map((brewery) => 
+            <Slider autoplay="1500" className="slider tour-slider">
+              {this.state.tour.breweries.map((brewery) =>
                 <div key={brewery.id}>
                   <Link to={`/breweries/${brewery.id}`} tour={this.props.match.params.id}><p className="tour-slider-name">{brewery.name}</p></Link>
                 </div>)}
             </Slider>
-            <button onClick={this.handleTourSubmit.bind(this)} className="btn button-pounce">Pounce on this tour!</button>&nbsp;&nbsp; 
+            <button onClick={this.handleTourSubmit.bind(this)} className="btn button-pounce">Pounce on this tour!</button>&nbsp;&nbsp;
             <button type="button" className="btn back-button"><Link className="back-link" to='/tours'>Back</Link></button>
           </span>
           <Map tips={this.state.tour} places={ this.state.tour.breweries } />
        </div>
        <Alert stack={{limit: 1}} />
+       <Footer><h6>9PM Productions</h6></Footer>
      </div>
 
        );
