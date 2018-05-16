82c8b97f521e92c2032111dcb682b951e184ad25
mattwong59 <mattwong59@gmail.com>
2018-05-16 00:40:49 +0000

    Move styling for footer to App.css

--- react_client/beercat_app/src/Footer.jsx
+++ react_client/beercat_app/src/Footer.jsx
@@ -1,30 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
 
-const footerStyle = {
-  backgroundColor: "#cccccc",
-  fontSize: "20px",
-  color: "black",
-  textAlign: "center",
-  padding: "20px",
-  left: "0",
-  bottom: "0",
-  height: "60px",
-  width: "100%"
-};
-
-const phantomStyle = {
-  display: "block",
-  padding: "20px",
-  height: "60px",
-  width: "100%"
-};
-
 function Footer({ children }) {
   return (
     <div>
-      <div style={phantomStyle} />
-      <div style={footerStyle}>{children}</div>
+      <div className= "phantomStyle" />
+      <div className="footerStyle">{children}</div>
     </div>
   );
 }
