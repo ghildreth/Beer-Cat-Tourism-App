c5d024d394a257a23817ce8de722507109b2bbe9
ghildreth <gabrielantonhildreth@gmail.com>
2018-05-16 03:16:53 +0000

    add style to singleUser page for beer preferences and remove avatar + seed data for SF tour

--- rails_server/beercat-server/app/controllers/maps_controller.rb
+++ rails_server/beercat-server/app/controllers/maps_controller.rb
@@ -5,7 +5,6 @@ class MapsController < ApplicationController
 
   def library
     uri = URI.parse("https://maps.googleapis.com/maps/api/js?key=#{ENV['GOOGLE_MAPS_KEY']}&v=3.31&libraries=places,geometry")
-    puts ENV['GOOGLE_MAPS_KEY']
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
