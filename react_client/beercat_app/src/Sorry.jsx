027a1371ebcc4b3fc02c397abbf1c9e99cccc1ae
ghildreth <gabrielantonhildreth@gmail.com>
2018-12-11 17:19:34 -0800

    changed href on button from /Under19 component

--- react_client/beercat_app/src/Sorry.jsx
+++ react_client/beercat_app/src/Sorry.jsx
@@ -9,7 +9,7 @@ export default class Under19 extends Component {
     <h1 className="sorrykitten">Sorry, kitten</h1>
     <img className="milk" src={process.env.PUBLIC_URL + '/assets_logo/Milk500.png'} alt="avatar"/>
     <h1>You can't come in right meow.</h1>
-    <a href="http://localhost:3000/over19"><button className="btn back-button">Back</button></a>    
+    <a href="./Over19"><button className="btn back-button">Back</button></a>    
   </div>);
   }
 }
